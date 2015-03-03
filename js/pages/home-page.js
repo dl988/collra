2b28793dcf130fe8f7df79ba7e1ca28d65d51bd8
linh.nguyen <linh.nguyen02@pyramid-consulting.com>
2015-03-03 16:14:11 +0700

    Restructure: Load modernizr from requireJs

--- js/pages/home-page.js
+++ js/pages/home-page.js
@@ -4,13 +4,16 @@
 			jQuery: '../vendor/jquery-1.10.2.min',
 			Handlebars: '../vendor/handlebars-v2.0.0',
 			CollraApi: '../collra/api',
-			CollraLoader: '../collra/loader'
+			CollraLoader: '../collra/loader',
+			Modernizr: '../vendor/modernizr-2.6.2.min',
+			Plugins: '../plugins'
 		}
 	});
 	
 	require([
-		'../plugins',
-		'../blocks/masonry'
+		'Plugins',
+		'../blocks/masonry',
+		'Modernizr'
 	], function(plugins, masonry){
 		masonry();
 	});
