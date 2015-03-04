cd7414fe7610fadae2fb91508799b73615a63fd3
linh.nguyen <linh.nguyen02@pyramid-consulting.com>
2015-03-04 09:35:27 +0700

    Improvement for block header: not fixed on the xsmall-layout

--- js/blocks/header.js
+++ js/blocks/header.js
@@ -1,6 +1,7 @@
 define([
-	'jQuery'
-], function(jQuery)
+	'Modernizr',
+	'jQuery',
+], function()
 {
 	
 	function Header()
@@ -9,9 +10,21 @@ define([
 		var $win = $(window);
 		var $header = $('.js-header');
 		
+		var checkIsXsmall = function()
+		{
+			if ($win.width() <= 768)
+			{
+				$header.removeClass('is-fixed');
+				$body.removeAttr('style');
+				return true;
+			}
+		}
+		
 		var update = function()
 		{
+			if(checkIsXsmall()) return;
 			
+			$header.addClass('is-fixed');
 			$body.css({'padding-top': $header.height() + 'px'});
 		}
 		
